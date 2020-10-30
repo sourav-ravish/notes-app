d415bbe345d92c6666199657ff806b0ca96308a8
sourav.ravish <sourav.ravish@fireeye.com>
2020-10-30 20:33:10 +0530

    updated code till add functionality

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const yargs = require('yargs')
+const notes = require('./notes')
 
 yargs.version('1.0.0')
 
 yargs.command({
     command: 'add',
     describe: 'Adds a note',
-    handler: function () {
-        console.log('What did you call me? Ok, I ll add a note for you.')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -15,6 +28,22 @@ yargs.command({
     describe: 'Removes a note',
     handler: function () {
         console.log('What really? Alright, I ll remove this note for you.')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists all notes',
+    handler: function () {
+        console.log('I ll list the notes for you.')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('I ll read a note, you tell me which one. ')
     }
 })
 
---
+++ notes.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const addNote = (title, body) =>  {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title
+    })
+
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse.bold('Note added successfully!'))
+    } else {
+        console.log(chalk.red.inverse.bold('Note already present!'))
+    }
+    
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataString = dataBuffer.toString()
+        return JSON.parse(dataString)
+    } catch(e){
+        return []
+    }
+}
+
+const saveNotes = function (notes) {
+    fs.writeFileSync('notes.json', JSON.stringify(notes))
+}
+
+module.exports = {
+    addNote: addNote
+}
